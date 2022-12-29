7918ee95429053490258d1ffa012038ffbd2150e
shivam shrivastava <shivam.shrivastava1006@gmail.com>
2022-12-29 19:09:32 +0530

    appbar created

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -7,9 +8,29 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ToDo App'),
-      ),
+          elevation: 0,
+          backgroundColor: tdBGColor,
+          title: Row(
+            children: [
+              const Icon(
+                Icons.menu,
+                color: tdBlack,
+                size: 30,
+              ),
+              Container(
+                height: 40,
+                width: 40,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset('assets/images/avatar.jpeg'),
+                ),
+              )
+            ],
+          )),
       body: const Text('data'),
     );
   }
 }
+
+// we used container -- which should be replaced with 
+    // sizedBox
